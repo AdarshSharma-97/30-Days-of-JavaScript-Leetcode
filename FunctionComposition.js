8d28532147c7d5bc89853bd701b2f621c3e2c7ca
Adarsh Sharma <adarshs861@gmail.com>
2025-01-04 15:52:19 +0530

    Day 8

---
+++ FunctionComposition.js
@@ -0,0 +1,16 @@
+// 2629. Function Composition
+var compose = function (functions) {
+  if (functions.length === 0) {
+    return function (x) {
+      return x;
+    };
+  } else {
+    return function (x) {
+      let result = x;
+      for (let i = functions.length - 1; i >= 0; i--) {
+        result = functions[i](result);
+      }
+      return result;
+    };
+  }
+};
